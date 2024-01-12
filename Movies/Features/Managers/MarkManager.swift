fddb76c80e84ae15687ee7d6b3b914fa33126cc0
Adam Cseke <69043287+adamcseke@users.noreply.github.com>
2024-01-12 07:50:12 +0100

    feat: MarkManager tests

--- Movies/Features/Managers/MarkManager.swift
+++ Movies/Features/Managers/MarkManager.swift
@@ -8,34 +8,6 @@
 import Foundation
 import Combine
 
-//protocol MarkManagerProtocol {
-//    func contains(_ movie: MovieVM) -> Bool
-//    func add(_ movie: MovieVM)
-//    func remove(_ movie: MovieVM)
-//}
-
-//class MarkManager: MarkManagerProtocol {
-//    var movies: Set<String>
-//
-//    init() {
-//        movies = []
-//    }
-//
-//    func contains(_ movie: MovieVM) -> Bool {
-//        movies.contains(movie.id)
-//    }
-//
-//    func add(_ movie: MovieVM) {
-////        objectWillChange.send()
-//        movies.insert(movie.id)
-//    }
-//
-//    func remove(_ movie: MovieVM) {
-////        objectWillChange.send()
-//        movies.remove(movie.id)
-//    }
-//}
-
 class MarkManager: ObservableObject {
     var movieIds: Set<String>
 
---
+++ MoviesTests/Managers/MarkManagerSpec.swift
@@ -0,0 +1,106 @@
+//
+//  MarkManagerSpec.swift
+//  MoviesTests
+//
+//  Created by Adam Cseke on 10/01/2024.
+//
+
+@testable import Movies
+import XCTest
+import Moya
+import Combine
+import Quick
+import Nimble
+import Mockingbird
+import Swinject
+import InjectPropertyWrapper
+
+class MarkManagerSpec: QuickSpec {
+
+    override func spec() {
+        describe("MarkManager") {
+            var sut: MarkManager!
+            var assembler: MainAssembler!
+
+            beforeEach {
+                assembler = MainAssembler.create(withAssemblies: [TestAssembly()])
+                InjectSettings.resolver = assembler.container
+                sut = assembler.resolver.resolve(MarkManager.self)
+            }
+
+            afterEach {
+                sut = nil
+                assembler.dispose()
+            }
+
+            context("when Mark Manager is initialized") {
+                var movie: MovieVM!
+
+                beforeEach {
+                    movie = MovieVM.any
+                    sut.movieIds = []
+                }
+
+                context("when no marked movie") {
+
+                    beforeEach {
+                        sut.movieIds = []
+                    }
+
+                    it("should have no movies") {
+                        expect(sut.movieIds.count).to(equal(0))
+                    }
+                }
+
+                context("when requested to mark a movie") {
+                    beforeEach {
+                        sut.add(movie)
+                    }
+
+                    it("checks if movie marked") {
+                        expect(sut.containsMovieId(movie.id)).to(beTrue())
+                    }
+                }
+
+                context("when requested to remove a movie") {
+                    beforeEach {
+                        sut.add(movie)
+                        sut.remove(movie)
+                    }
+
+                    it("checks if movie marked") {
+                        expect(sut.containsMovieId(movie.id)).to(beFalse())
+                    }
+                }
+
+                context("when a movie is added or removed") {
+                    it("should send an update through objectDidChange") {
+                        var changeFired = false
+                        let cancellable = sut.objectDidChange.sink { changeFired = true }
+
+                        sut.add(movie)
+                        expect(changeFired).to(beTrue())
+
+                        changeFired = false
+                        sut.remove(movie)
+                        expect(changeFired).to(beTrue())
+
+                        cancellable.cancel()
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension MarkManagerSpec {
+    class TestAssembly: Assembly {
+
+        func assemble(container: Container) {
+
+            container.register(MarkManager.self) { _ in
+                return MarkManager()
+            }.inObjectScope(.transient)
+        }
+    }
+}
